0ba6f99c38ea2bcafbf0c5c89513443f5d987e75
gorwen <gael.gubian@gmail.com>
2016-10-02 00:37:17 +0200

    idem

--- backend/src/main/java/fr/fhacktory/model/Sentence.java
+++ backend/src/main/java/fr/fhacktory/model/Sentence.java
@@ -3,6 +3,7 @@ package fr.fhacktory.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.Data;
 @Entity
 public class Sentence implements Serializable {
 	@Id
+	@GeneratedValue
 	private Integer id;
 	private String sentence;
 }
