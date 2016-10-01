3632358fc61ccd6280029fca305542433dbcef23
Aurélien Mino <aurelien.mino@gmail.com>
2016-10-01 14:04:34 +0200

    Init du backend.

---
+++ backend/src/main/java/fr/fhacktory/MyStoryTellerApplication.java
@@ -0,0 +1,12 @@
+package fr.fhacktory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoryTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoryTellerApplication.class, args);
+	}
+}
