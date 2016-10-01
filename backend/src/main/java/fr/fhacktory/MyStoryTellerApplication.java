4115baeec2ff0423f8776e6791f041ae633fce95
gorwen <gael.gubian@gmail.com>
2016-10-01 17:59:09 +0200

    Génération de phrase

--- backend/src/main/java/fr/fhacktory/MyStoryTellerApplication.java
+++ backend/src/main/java/fr/fhacktory/MyStoryTellerApplication.java
@@ -2,6 +2,9 @@ package fr.fhacktory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyStoryTellerApplication {
@@ -9,4 +12,12 @@ public class MyStoryTellerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyStoryTellerApplication.class, args);
 	}
+	
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	
 }
--- backend/src/main/java/fr/fhacktory/nlg/PhraseGenerator.java
+++ backend/src/main/java/fr/fhacktory/nlg/PhraseGenerator.java
@@ -1,25 +1,44 @@
 package fr.fhacktory.nlg;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import simplenlg.framework.NLGFactory;
+import simplenlg.lexicon.Lexicon;
+import simplenlg.phrasespec.SPhraseSpec;
+import simplenlg.realiser.english.Realiser;
+
 @RestController
 public class PhraseGenerator {
 
-	 private static final String template = "Hello, %s!";
+	private NLGFactory nlgFactory;
+	private Realiser realiser;
+	private static final String template = "Hello, %s!";
 
-	    @RequestMapping("/greeting")
-	    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	        return String.format(template, name);
-	    }
-	    
-	    
-	    @RequestMapping("/sentenceGenerator")
-	    public String generateSentence(@RequestParam(value="subject", defaultValue="Toto") String subject, @RequestParam(value="verb", defaultValue="Manger") String verb, @RequestParam(value="complement", defaultValue="pomme") String complement) {
-	        return String.format("%s %s %s", subject, verb, complement);
-	    }
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format(template, name);
+	}
 
-	    
+	@RequestMapping("/sentenceGenerator")
+	public String generateSentence(@RequestParam(value = "subject", defaultValue = "Toto") String subject,
+			@RequestParam(value = "verb", defaultValue = "Manger") String verb,
+			@RequestParam(value = "complement", defaultValue = "pomme") String complement) {
+		SPhraseSpec p = nlgFactory.createClause();
+		p.setSubject(subject);
+		p.setVerb(verb);
+		p.setObject(complement);
+		return this.realiser.realiseSentence(p);
+	}
+
+	public PhraseGenerator() {
+		super();
+		Lexicon lexicon = Lexicon.getDefaultLexicon();
+		this.nlgFactory = new NLGFactory(lexicon);
+		this.realiser = new Realiser(lexicon);
+	}
+
 }
-	
