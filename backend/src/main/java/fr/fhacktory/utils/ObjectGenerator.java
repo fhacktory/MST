a07752a6f2f1b75b28026a0426ff7ef099d36e79
mohamed44Y <yangui.med@gmail.com>
2016-10-02 11:09:19 +0200

    adding some objects to ObjectGenerator

--- backend/src/main/java/fr/fhacktory/utils/ObjectGenerator.java
+++ backend/src/main/java/fr/fhacktory/utils/ObjectGenerator.java
@@ -9,10 +9,16 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ObjectGenerator {
 
-    private static final List<String> objects = Arrays.asList("glace","téléphone","jus","banane", "baby foot", "souris", "fleurs", "cerises", " diamant","dragon", "mirroir");
-    
-    
-    public static String getAnObject(){
+    private static final List<String> objects = Arrays.asList("glace", "téléphone", "jus", "banane", "baby foot",
+	    "souris", "fleurs", "cerises", " diamant", "dragon", "mirroir", "Avion", "Ballon", "Bateau",
+	    "Brosse à dent", "Camion", "Camion pompier", "Canapé", "Ceinture", "Chaise", "Chaussette", "Cheminée",
+	    "Cirque", "Ciseau", "Coffre-fort", "Disque", "Distributeur", "Drap", "Flipper", "Fourchette", "Frigo",
+	    "Igloo", "Jeu de cartes", "Lampe", "Lit", "Maison", "Mer", "Montagne", "Neige", "Ordinateur",
+	    "Porte-monnaie", "Poubelle", "Prise éléctrique", "Raquette", "Slip", "Stylo", "Table", "Tam-tam",
+	    "Tablette tactile", "Tee-shirt", "Télécommande", "Téléphone", "Télévision", "Tour Eiffel", "Valise",
+	    "Voiture", "Walkman");
+
+    public static String getAnObject() {
 	Random rand = new Random();
 	return objects.get(rand.nextInt(objects.size()));
     }
